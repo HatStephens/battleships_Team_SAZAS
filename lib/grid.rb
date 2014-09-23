758a21be07617b0faf14e91f3677457fe2ea2baa
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-23 15:59:24 +0100

    Can place a ship. Yeah.

--- lib/grid.rb
+++ lib/grid.rb
@@ -4,12 +4,13 @@ class Grid
 		# @row_array = Array.new(10)
 		# @plus_array = Array.new(10,'+')
 		# @grid = @row_array.each.with_index { |element, i| @row_array[i]=@plus_array.join(' ') }
-		@grid = Array.new(10) {Array.new(10, '+').join(' ')}
+		@grid = Array.new(10) {Array.new(10, '+')}
 		@received_ships = []
 	end
 
 	def display_grid
-		puts @grid
+		(0..9).each { |n| puts @grid[n].join(' ')}
+		# @grid
 	end
 
 	def received_ships
@@ -20,8 +21,11 @@ class Grid
 		@received_ships << ship
 	end
 
+	def place_ship_horizontally(ship, start_row, start_col)
+		@grid[start_row-1][start_col-1] = ship.body[0]
+		@grid[start_row-1][start_col] = ship.body[1]
+		@grid[start_row-1][start_col+1] = ship.body[2]
+	end
+
 
 end
-
-grid = Grid.new
-print grid.display_grid
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -16,8 +16,12 @@ describe Grid do
 		expect(grid.received_ships).to eq [ship]
 	end
 
-	it 'should be able to place a ship on the grid' do
-
+	it 'should be able to place a ship on the grid horizontally' do
+		allow(ship).to receive(:body).and_return(['S','S','S'])
+		grid.place_ship_horizontally(ship, 4, 6)
+		expect(grid.display_grid[3][5]).to eq('S')
+		expect(grid.display_grid[3][6]).to eq('S')
+		expect(grid.display_grid[3][7]).to eq('S')
 	end
 
 	
