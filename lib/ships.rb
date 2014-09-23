c9c46ce0207fc2ba78a6920e450628ad989504e1
ananogal <ana.nogal@gmail.com>
2014-09-23 15:23:45 +0100

    refactoring ship class

--- lib/ships.rb
+++ lib/ships.rb
@@ -11,4 +11,19 @@ class Ship
 		@size.times{@body << @name[0].upcase}
 	end
 
+	def self.destroyer
+		new(:destryer, 2)
+	end
+
+	def self.submarine
+		new(:submarine, 3)
+	end
+
+	def self.battleship
+		new(:battleship, 4)
+	end
+
+	def self.aircraft
+		new(:aicraft, 5)
+	end
 end
