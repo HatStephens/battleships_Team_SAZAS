313a59312b4a99588e27f167155b6464674aecd4
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-23 16:15:17 +0100

    We can any ship horizontally.

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,30 +1,26 @@
 class Grid
 
+	attr_reader :grid
+
 	def initialize
-		# @row_array = Array.new(10)
-		# @plus_array = Array.new(10,'+')
-		# @grid = @row_array.each.with_index { |element, i| @row_array[i]=@plus_array.join(' ') }
 		@grid = Array.new(10) {Array.new(10, '+')}
 		@received_ships = []
 	end
 
-	def display_grid
+	def print_grid
 		(0..9).each { |n| puts @grid[n].join(' ')}
-		# @grid
 	end
 
 	def received_ships
 		@received_ships
 	end
 
-	def receive(ship)
-		@received_ships << ship
-	end
+	# def receive(ship)
+	# 	@received_ships << ship
+	# end
 
 	def place_ship_horizontally(ship, start_row, start_col)
-		@grid[start_row-1][start_col-1] = ship.body[0]
-		@grid[start_row-1][start_col] = ship.body[1]
-		@grid[start_row-1][start_col+1] = ship.body[2]
+		(0..ship.size).each {|n| @grid[start_row-1][start_col+(-1+n)] = ship.body[n]}
 	end
 
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,21 +7,22 @@ describe Grid do
 	let(:ship) {double :ship}
 
 	it 'should create a 10 by 10 array full of + signs when created' do
-		expect(grid.display_grid[3][5]).to eq('+')  
+		expect(grid.grid[3][5]).to eq('+')  
 	end
 
-	it "should know what ships need to be placed on the grid" do
-		expect(grid.received_ships).to eq []
-		grid.receive(ship)
-		expect(grid.received_ships).to eq [ship]
-	end
+	# it "should know what ships need to be placed on the grid" do
+	# 	expect(grid.received_ships).to eq []
+	# 	grid.receive(ship)
+	# 	expect(grid.received_ships).to eq [ship]
+	# end
 
 	it 'should be able to place a ship on the grid horizontally' do
 		allow(ship).to receive(:body).and_return(['S','S','S'])
+		allow(ship).to receive(:size).and_return(3)
 		grid.place_ship_horizontally(ship, 4, 6)
-		expect(grid.display_grid[3][5]).to eq('S')
-		expect(grid.display_grid[3][6]).to eq('S')
-		expect(grid.display_grid[3][7]).to eq('S')
+		expect(grid.grid[3][5]).to eq('S')
+		expect(grid.grid[3][6]).to eq('S')
+		expect(grid.grid[3][7]).to eq('S')
 	end
 
 	
